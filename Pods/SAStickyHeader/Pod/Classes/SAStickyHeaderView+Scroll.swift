c3559289d3704129d973fe491a4485a793b51ffb
Igor Kuznetsov <iforaa@yandex.ru>
2016-06-24 18:37:53 +0300

    Pod updates

---
+++ Pods/SAStickyHeader/Pod/Classes/SAStickyHeaderView+Scroll.swift
@@ -0,0 +1,33 @@
+//
+//  SAStickyHeaderView+Scroll.swift
+//  SA
+//
+//  Created by shams ahmed on 18/10/2015.
+//
+//
+
+import UIKit
+
+extension SAStickyHeaderView {
+
+    // MARK:
+    // MARK: Scroll
+
+    /// Update header view from a drag movement
+    func updateImageViewFromScrollEvent(tableView: UITableView) {
+        // if current view is not self, just return to avoid any glitch 
+//        if !isTouchingView {
+//            return
+//        }
+        
+        let insert = tableView.contentInset
+        let offset = -(tableView.contentOffset.y + insert.top)
+        
+        containerLayoutConstraint.constant = insert.top
+        
+        containerView.clipsToBounds = offset <= 0
+
+        bottomLayoutConstraint.constant = offset >= 0 ? 0 : -offset / 2
+        heightLayoutConstraint.constant = max(offset + insert.top, insert.top)
+    }
+}
---
+++ Pods/Target Support Files/SAStickyHeader/SAStickyHeader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SAStickyHeader : NSObject
+@end
+@implementation PodsDummy_SAStickyHeader
+@end
